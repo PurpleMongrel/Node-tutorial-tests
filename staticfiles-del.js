a5b6121fa382f05fcbc9959ef42adf0c07309690
PurpleMongrel <thaispdf@gmail.com>
2022-04-22 23:59:35 -0400

    tutorial deletable

--- staticfiles-del.js
+++ staticfiles-del.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const server = http.createServer((req,res) => {
-  const readStream = fs.createReadStream('./static/b-del.js');
-  res.writeHead(200,{'Content-Type': 'application/javascript'});
+  const readStream = fs.createReadStream('./static/cryptoDilo.png');
+  res.writeHead(200,{'Content-Type': 'image/png'});
   readStream.pipe(res);
 }).listen(3000);
