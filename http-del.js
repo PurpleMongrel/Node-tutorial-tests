12de784888e0cf383aa959093435165a3fd7ca86
PurpleMongrel <thaispdf@gmail.com>
2022-04-22 23:13:47 -0400

    ...

--- http-del.js
+++ http-del.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req,res) => {
    if (req.url === '/'){
-      
+      res.write('Bob Dilo')
       res.end();
    }
    else {
--- staticfiles-del.js
+++ staticfiles-del.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const server = http.createServer((req,res) => {
-  const readStream = fs.createReadStream('./static/statictest-del.html');
-  res.writeHead(200,{'Content-Type' = 'text/html'});
-
+  const readStream = fs.createReadStream('./static/b-del.js');
+  res.writeHead(200,{'Content-Type': 'application/javascript'});
+  readStream.pipe(res);
 }).listen(3000);
